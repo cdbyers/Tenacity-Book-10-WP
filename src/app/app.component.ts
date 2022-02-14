7031dae526a4403c2f8f036b399c841738e904ef
Joseph Edusei <jedusei99@gmail.com>
2022-02-14 09:45:11 +0000

    completed toolbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'etlearns-book';
+  isFullscreen = false;
+
+  ngOnInit() {
+    this.isFullscreen = !!document.fullscreenElement;
+    document.addEventListener('fullscreenchange', this.fullscreenChangeHandler);
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener(
+      'fullscreenchange',
+      this.fullscreenChangeHandler
+    );
+  }
+
+  toggleFullscreen() {
+    if (this.isFullscreen) document.exitFullscreen();
+    else document.documentElement.requestFullscreen();
+  }
+ 
+  fullscreenChangeHandler = (() => {
+    this.isFullscreen = !!document.fullscreenElement;
+  }).bind(this);
 }
