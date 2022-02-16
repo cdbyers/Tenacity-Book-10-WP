01c9109f9c9abfa68b567a705d4e5fd53329aeb8
Joseph Edusei <jedusei99@gmail.com>
2022-02-16 14:15:09 +0000

    added page 8.

--- src/app/pages.ts
+++ src/app/pages.ts
@@ -12,6 +12,7 @@ const pages: Page[] = [
   { title: 'Unit 1 - Sport and fitness' },
   { title: 'Speaking & Listening' },
   { title: 'Reading: East African athletes (Part 1)' },
+  { title: 'Reading: East African athletes (Part 2)' },
 ];
 
 export default pages;
